6a6b54cba21586086c9a5ea09e259a9e93423393
Luca Carrozzo <carrozzo.lc@gmail.com>
2022-11-13 16:44:26 +0100

    factorial recursion done

--- specs/recursion/factorials.test.js
+++ specs/recursion/factorials.test.js
@@ -8,13 +8,27 @@
   factorial(1) = 1
   factorial(2) = 2
   factorial(3) = 6 
+
+  Teacher solution
+  function factorial(n) {
+    if (n < 2) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }  
 */
 
-function factorial(n) {}
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
 
 // unit tests
 // do not modify the below code
-test.skip("factorials", () => {
+test("factorials", () => {
   expect(factorial(1)).toEqual(1);
   expect(factorial(2)).toEqual(2);
   expect(factorial(3)).toEqual(6);
