dda29e79f09c50a2819db4f2eda08e24560d8860
Luca Carrozzo <carrozzo.lc@gmail.com>
2022-11-13 16:44:03 +0100

    nested array recursion done

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -6,15 +6,43 @@
 
   example: nestedAdd([1, 2, [3]]) = 6
            nestedAdd([[[2]], 1, [1, 3]]) = 7
+
+
+  // teacher solution
+  function nestedAdd(array) {
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      const current = array[i];
+      if (Array.isArray(current)) {
+        sum += nestedAdd(current);
+      } else {
+        sum += current;
+      }
+    }
+
+    return sum;
+  }
  
  */
 
 function nestedAdd(array) {
-  // write code here
+  let result = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'number') {
+      result = result + array[i]; 
+    } else {
+      result = result + nestedAdd(array[i]);
+    }
+  }
+
+  return result;
 }
 
-test.skip("nested arrays addition", () => {
+test("nested arrays addition", () => {
   expect(nestedAdd([1, 2, 3])).toEqual(6);
+  expect(nestedAdd([[[2]], 1, [1, 3]])).toEqual(7);
   expect(nestedAdd([1, [2], 3])).toEqual(6);
   expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
   expect(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11])).toEqual(94);
