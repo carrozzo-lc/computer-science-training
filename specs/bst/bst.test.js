7f60923474a3fca69315832f18fd43c1c2a5c0b6
Luca Carrozzo <carrozzo.lc@gmail.com>
2022-12-27 08:55:19 +0100

    binary search trees done

--- specs/bst/bst.test.js
+++ specs/bst/bst.test.js
@@ -17,17 +17,54 @@ right - Node/object - the right node which itself may be another tree
 */
 
 class Tree {
-  // code goes here
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    const node = new Node(value);
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = node;
+            break;
+          } else {
+            current = current.left;
+          }
+        } else {
+          if (current.right === null) {
+            current.right = node;
+            break;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+    return this;
+  }
+
+  toObject() {
+    return this.root;
+  }
 }
 
 // you might consider using a Node class too
-// class Node {
-//   // code maybe goes here
-// }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
 // unit tests
 // do not modify the below code
-describe.skip("Binary Search Tree", function () {
+describe("Binary Search Tree", function () {
   it("creates a correct tree", () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
     const tree = new Tree();
